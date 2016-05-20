9a3972598b30798c934708335e94e6d76e54eda2
Markku Riekkinen <markku.riekkinen@aalto.fi>
2016-05-20 18:59:57 +0300

    Remove the separate plain exercise page (AJAX) (now included in the normal script).

--- stratumtwo/classes/urls/urls.php
+++ stratumtwo/classes/urls/urls.php
@@ -52,12 +52,6 @@ class urls {
         return self::buildUrl('/exercise.php', $query, $asMdlUrl);
     }
     
-    // plain exercise for embedding into chapters (AJAX)
-    public static function plainExercise(\mod_stratumtwo_learning_object $ex, $asMdlUrl = false) { //TODO remove?
-        $query = array('id' => $ex->getId());
-        return self::buildUrl('/exercise_plain.php', $query, $asMdlUrl);
-    }
-    
     public static function editExercise(\mod_stratumtwo_learning_object $ex, $asMdlUrl = false) {
         $query = array('id' => $ex->getId());
         return self::buildUrl('/teachers/edit_exercise.php', $query, $asMdlUrl);
