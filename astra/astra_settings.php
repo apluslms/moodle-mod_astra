c34e106f531291e0b35322bbe633db24ba350a3b
Markku Riekkinen <markku.riekkinen@aalto.fi>
2016-08-24 11:28:15 +0300

    Change constant name (plugin name change).

--- astra/astra_settings.php
+++ astra/astra_settings.php
@@ -4,4 +4,4 @@
  * Secret key is used to generate keyed hash values for the asynchronous
  * exercise service API.
  */
-define('STRATUMTWO_SECRET_KEY', 'ej8sNlpV8kUQbZkcjVJbRwJpTZqqcUwsEI5cZeq1vta0FSyzlaDo2Z_UhG-9_N7Yx8_0C-yVTeVIWird8nqzJhcOPwv6fs-fSO-');
+define('ASTRA_SECRET_KEY', 'ej8sNlpV8kUQbZkcjVJbRwJpTZqqcUwsEI5cZeq1vta0FSyzlaDo2Z_UhG-9_N7Yx8_0C-yVTeVIWird8nqzJhcOPwv6fs-fSO-');
