ba43b17a34aac08d77425d45cbea30af78427e25
Markku Riekkinen <markku.riekkinen@aalto.fi>
2017-10-20 21:42:25 +0300

    Update version.

--- astra/version.php
+++ astra/version.php
@@ -14,8 +14,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_astra';
-$plugin->version = 2017101700;
-$plugin->release = 'v1.2';
+$plugin->version = 2017102000;
+$plugin->release = 'v1.3';
 $plugin->requires =  2016120500; // Moodle 3.2
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
