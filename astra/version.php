9f93bc16e543e5023d1caeff108ba4a3dc0f6024
Markku Riekkinen <markku.riekkinen@aalto.fi>
2018-12-17 17:42:50 +0200

    Release v1.6.1

--- astra/version.php
+++ astra/version.php
@@ -14,8 +14,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_astra';
-$plugin->version = 2018120400;
-$plugin->release = 'v1.6.0';
+$plugin->version = 2018121700;
+$plugin->release = 'v1.6.1';
 $plugin->requires = 2017111300; // Moodle 3.4
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
